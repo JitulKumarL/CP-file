c9bbbd779b40e188d173b72705d2023c51f379d7
JitulKumarL <80389538+JitulKumarL@users.noreply.github.com>
2021-06-04 12:27:04 +0530

    Update cp.sh

--- cp.sh
+++ cp.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-DIR="/home/$(hostname -f)/aws-live"
+DIR="/home/ubuntu/aws-live"
 if [ -d "$DIR" ]; then
   ### Take action if $DIR exists ###
-  cd /home/$(hostname -f)/aws-live
-  echo "Installing config files in ${DIR}..."
+  cd /home/ubuntu/aws-live
+  sudo chmod +x EmpApp.py
+  sudo nohup /home/ubuntu/aws-live/EmpApp.py &
+  ##echo "Installing config files in ${DIR}..."##
 else
   ###  Control will jump here if $DIR does NOT exists ###
   git clone https://github.com/JitulKumarL/aws-live.git
-  cd /home/$(hostname -f)/aws-live
+  cd /home/ubuntu/aws-live
+  sudo chmod +x EmpApp.py
+  sudo nohup /home/ubuntu/aws-live/EmpApp.py &
+  
   exit 1
 fi
